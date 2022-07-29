a437eff2db3232615e84681dc46538450014b136
soffer1 <soffer1@mail.tau.ac.il>
2022-07-29 18:08:59 +0300

    V1.2

--- Python_Project/Player.py
+++ Python_Project/Player.py
@@ -5,10 +5,8 @@ import random
 
 def parse_player_line(line):
     arguments = line.strip().split(', ')
-    return g_vs.COLOR_DICT[arguments[0]], \
-           g_vs.KEYS_DICT[arguments[1]], \
-           g_vs.KEYS_DICT[arguments[2]], \
-           g_vs.KEYS_DICT[arguments[3]]
+    return g_vs.COLOR_DICT[arguments[0]], g_vs.KEYS_DICT[arguments[1]], \
+        g_vs.KEYS_DICT[arguments[2]], g_vs.KEYS_DICT[arguments[3]]
 
 
 class Player:
